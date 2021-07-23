5bc9fee79b7878c9a98616ea1cd24122a3f0dbcd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-23 23:54:27 +0100

    Add src/main/scala/com/grey/DataSteps.scala

---
+++ src/main/scala/com/grey/DataSteps.scala
@@ -0,0 +1,19 @@
+package com.grey
+
+import org.apache.spark.sql.SparkSession
+
+class DataSteps(spark: SparkSession) {
+
+    def dataSteps(): Unit = {
+
+        /**
+         * Import implicits for
+         *   encoding (https://jaceklaskowski.gitbooks.io/mastering-apache-spark/spark-sql-Encoder.html)
+         *   implicit conversions, e.g., converting a RDD to a DataFrames.
+         *   access to the "$" notation.
+         */
+        import spark.implicits._
+
+    }
+    
+}
