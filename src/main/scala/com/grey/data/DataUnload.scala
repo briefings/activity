64f1a0bfd9fe7f91962c9a4344e425166cf2f208
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-23 23:49:21 +0100

    Update src/main/scala/com/grey/data/DataUnload.scala

--- src/main/scala/com/grey/data/DataUnload.scala
+++ src/main/scala/com/grey/data/DataUnload.scala
@@ -1,5 +1,36 @@
 package com.grey.data
 
-class Unload {
+import com.grey.net.IsExistURL
+
+import java.io.File
+import java.net.URL
+import scala.language.postfixOps
+import scala.sys.process._
+import scala.util.Try
+import scala.util.control.Exception
+
+class DataUnload {
+
+
+    def dataUnload(urlString: String, unloadString: String): Try[String] = {
+
+        // Valid URL?
+        val isExistURL: Try[Boolean] = new IsExistURL().isExistURL(urlString = urlString)
+
+        // Unload
+        val unload: Try[String] = if (isExistURL.isSuccess) {
+            Exception.allCatch.withTry(
+                new URL(urlString) #> new File(unloadString) !!
+            )
+        } else {
+            sys.error(isExistURL.failed.get.getMessage)
+        }
+
+        // Hence
+        unload
+
+    }
+
+
 
 }
