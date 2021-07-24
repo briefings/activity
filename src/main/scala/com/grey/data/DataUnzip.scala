87540b182860aeb67c5387ab0161cc740f245c6d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-24 18:09:26 +0100

    Update src/main/scala/com/grey/data/DataUnzip.scala

--- src/main/scala/com/grey/data/DataUnzip.scala
+++ src/main/scala/com/grey/data/DataUnzip.scala
@@ -1,57 +1,65 @@
 package com.grey.data
 
-import com.grey.environment.LocalSettings
-
 import java.io.{File, FileInputStream, FileOutputStream}
 import java.util.zip.ZipInputStream
-
+import scala.util.Try
+import scala.util.control.Exception
 
 /**
  * De-archives '.zip' files
+ *
+ * @param directory: The directory wherein the archive extracts will be saved
  */
-class DataUnzip {
-
-    private val localSettings = new LocalSettings()
-    private val dataDirectory = localSettings.dataDirectory
-    private val archiveDirectory = localSettings.archiveDirectory
+class DataUnzip(directory: String) {
 
     /**
      *
-     * https://www.baeldung.com/java-compress-and-uncompress
-     * @param archiveString: The name of a zip file
+     * @param archiveString: The name of a zip file; it must include the path to its location an its extension
+     * @return Boolean -> Unzipped successfully?
      */
-    def dataUnzip(archiveString: String): Unit = {
+    def dataUnzip(archiveString: String): Boolean = {
 
         // Buffer
         val buffer = new Array[Byte](2048)
 
         // Create an archive input stream object
-        val zipInputStream =  new ZipInputStream(new FileInputStream(archiveDirectory + archiveString))
+        val zipInputStream =  new ZipInputStream(new FileInputStream(archiveString))
         var zipEntry = zipInputStream.getNextEntry
 
         // Whilst a file exists in the archive object
-        while (zipEntry != null) {
+        val dearchive: Try[Unit] = Exception.allCatch.withTry(
 
-            // The next file in the archive
-            val fileName = zipEntry.getName
-            val fileObject = new File(dataDirectory + fileName)
+            while (zipEntry != null) {
 
-            // For cases whereby the archive has a directory structure
-            if (!new File(fileObject.getParent).exists()) {
-                new File(fileObject.getParent).mkdirs()
+                // The next file in the archive
+                val fileName = zipEntry.getName
+                val fileObject = new File(directory + fileName)
+
+                // For cases whereby the archive has a directory structure
+                if (!new File(fileObject.getParent).exists()) {
+                    new File(fileObject.getParent).mkdirs()
+                }
+
+                // Herein, the file's contents are saved to disk
+                val fileOutputStream = new FileOutputStream(fileObject)
+                while (zipInputStream.read(buffer) > 0) {
+                    fileOutputStream.write(buffer, 0, zipInputStream.read(buffer))
+                }
+                fileOutputStream.close()
+                zipEntry = zipInputStream.getNextEntry
+
             }
 
-            // Herein, the file's contents are saved to disk
-            val fileOutputStream = new FileOutputStream(fileObject)
-            while (zipInputStream.read(buffer) > 0) {
-                fileOutputStream.write(buffer, 0, zipInputStream.read(buffer))
-            }
-            fileOutputStream.close()
-            zipEntry = zipInputStream.getNextEntry
-
-        }
+        )
         zipInputStream.closeEntry()
         zipInputStream.close()
+
+        // Status
+        if (dearchive.isSuccess){
+            dearchive.isSuccess
+        } else {
+            sys.error(dearchive.failed.get.getMessage)
+        }
 
     }
 
