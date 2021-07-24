71baa22bfbba25083c0861a57755de6bf7cd667f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-24 18:08:23 +0100

    Update src/main/scala/com/grey/ActivityApp.scala

--- src/main/scala/com/grey/ActivityApp.scala
+++ src/main/scala/com/grey/ActivityApp.scala
@@ -1,16 +1,26 @@
 package com.grey
 
 import com.grey.environment.{Directories, LocalSettings}
+import com.grey.net.InspectArguments
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 import scala.collection.parallel.immutable.ParSeq
+import scala.util.Try
 
 object ActivityApp {
 
     private val localSettings = new LocalSettings()
 
     def main(args: Array[String]): Unit = {
+
+        // Arguments
+        if (args.length == 0) {
+            sys.error("The data URL is required.")
+        }
+
+        val inspectArguments = InspectArguments
+        val (isURL: Try[Boolean], urlString: String) = inspectArguments.inspectArguments(args = args)
 
         // Minimising log information output
         Logger.getLogger("org").setLevel(Level.ERROR)
@@ -30,7 +40,7 @@ object ActivityApp {
 
         // Hence
         if (directoriesReset.forall(_ == true)) {
-            new DataSteps(spark = spark)
+            new DataSteps(spark = spark).dataSteps(urlString = urlString)
         } else {
             // Superfluous because directoriesReset( ) address all directoriesReset( ) errors
             sys.exit()
