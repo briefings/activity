7ef1098e286bd81b3999717d63e5fa5b2d6fdd85
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-07-25 00:09:27 +0100

    Update src/main/scala/com/grey/ActivityApp.scala

--- src/main/scala/com/grey/ActivityApp.scala
+++ src/main/scala/com/grey/ActivityApp.scala
@@ -1,12 +1,10 @@
 package com.grey
 
 import com.grey.environment.{Directories, LocalSettings}
-import com.grey.net.InspectArguments
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
 import scala.collection.parallel.immutable.ParSeq
-import scala.util.Try
 
 object ActivityApp {
 
@@ -15,12 +13,17 @@ object ActivityApp {
     def main(args: Array[String]): Unit = {
 
         // Arguments
+        /*
         if (args.length == 0) {
             sys.error("The data URL is required.")
         }
+        */
 
+        // Inspect Arguments
+        /*
         val inspectArguments = InspectArguments
-        val (isURL: Try[Boolean], urlString: String) = inspectArguments.inspectArguments(args = args)
+        val (_, urlString: String) = inspectArguments.inspectArguments(args = args)
+        */
 
         // Minimising log information output
         Logger.getLogger("org").setLevel(Level.ERROR)
@@ -35,12 +38,12 @@ object ActivityApp {
 
         // Prepare local directories
         val directories = new Directories()
-        val directoriesReset: ParSeq[Boolean] = List(localSettings.archiveDirectory, localSettings.dataDirectory,
-            localSettings.warehouseDirectory).par.map( directory => directories.directoryReset(directory) )
+        val directoriesReset: ParSeq[Boolean] = List(localSettings.warehouseDirectory).
+            par.map(directory => directories.directoryReset(directory))
 
         // Hence
         if (directoriesReset.forall(_ == true)) {
-            new DataSteps(spark = spark).dataSteps(urlString = urlString)
+            new DataSteps(spark = spark).dataSteps()
         } else {
             // Superfluous because directoriesReset( ) address all directoriesReset( ) errors
             sys.exit()
